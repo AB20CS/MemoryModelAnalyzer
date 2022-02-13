b05ec7fc7ce243d686ddbd1e260cdb59cf01d39c
Anindro Bhattacharya <69637288+AB20CS@users.noreply.github.com>
2022-02-13 18:31:37 -0500

    Update SimpleMemModAnalyzer.c

--- SimpleMemModAnalyzer.c
+++ SimpleMemModAnalyzer.c
@@ -42,13 +42,13 @@ typedef struct LinkedListHeads {
  * Inserts node into linked list whose head is head
  * 
  */
-void insertFuncNode(LLHeads* heads, FunctionNode *node) {
+void insertFuncNode(FunctionNode *func_head, FunctionNode *node) {
 
-    if (heads->func_head == NULL) {
-        heads->func_head = node;
+    if (func_head == NULL) {
+        func_head = node;
     }
     else {
-        FunctionNode *p = heads->func_head;
+        FunctionNode *p = func_head;
         while (p->next != NULL) {
             p = p->next;
         }
@@ -75,6 +75,58 @@ void insertMemNode(MemNode *head, MemNode *node) {
     
 }
 
+/**
+ * Deletes a FunctionNode linked list whose head is head
+ * 
+ */
+void deleteFunctionList(FunctionNode *head) {
+    FunctionNode *p = head;
+    FunctionNode *q = NULL;
+
+    while (p != NULL) {
+        q = p->next;
+        free(p);
+        p = q;
+    }
+}
+
+/**
+ * Returns the size of memory occupied by a type
+ * 
+ */
+int getSize(char *type, int num_elements) {
+    int size_per_element;
+    if (strcmp(type, "int") == 0) {
+        size_per_element = sizeof(int);
+    }
+    else if (strcmp(type, "float") == 0) {
+        size_per_element = sizeof(float);
+    }
+    else if (strcmp(type, "char") == 0) {
+        size_per_element = sizeof(char);
+    }
+    else if (strcmp(type, "int *") == 0 || strcmp(type, "int*") == 0) {
+        size_per_element = sizeof(int*);
+    }
+    else if (strcmp(type, "float *") == 0 || strcmp(type, "float*") == 0) {
+        size_per_element = sizeof(float*);
+    }
+    else if (strcmp(type, "char *") == 0 || strcmp(type, "char*") == 0) {
+        size_per_element = sizeof(char*);
+    }
+    else if (strcmp(type, "int **") == 0 || strcmp(type, "int**") == 0) {
+        size_per_element = sizeof(int**);
+    }
+    else if (strcmp(type, "float **") == 0 || strcmp(type, "float**") == 0) {
+        size_per_element = sizeof(float**);
+    }
+    else if (strcmp(type, "char **") == 0 || strcmp(type, "char**") == 0) {
+        size_per_element = sizeof(char**);
+    }
+
+    return size_per_element * num_elements;
+}
+
 
 /**
  * Returns true iff line is a function header
@@ -99,7 +151,8 @@ bool isFunctionHeader(char *line, char **types, int num_types) {
  * if a variable is contained
  * 
  */
-bool isVar(char *line, char **types, int num_types, FunctionNode *curr_func, MemNode *head) {
+bool isVar(char *line, char **types, int num_types, FunctionNode *curr_func, 
+           MemNode *ro_head, MemNode *static_head, MemNode *heap_head, MemNode *stack_head) {
     
     bool contains_var = false;
 
@@ -111,7 +164,7 @@ bool isVar(char *line, char **types, int num_types, FunctionNode *curr_func, Mem
     char line_copy[1024]; // copy of line for tokenization
     strcpy(line_copy, line);
     char *type = strtok(line_copy, " ");
-
+    
     strcat(type, " ");
     
     for (int i = 0; i < num_types; i++) {
@@ -121,30 +174,99 @@ bool isVar(char *line, char **types, int num_types, FunctionNode *curr_func, Mem
         }
     }
 
-    if (contains_var) {
-        // modify type field
-        if (strstr(line, "[") && strstr(line, "]")) { // if variable is array
-            strcat(type, "[]");
-        }
-        else { // remove added space
-            type[strlen(type) - 1] = '\0';
+    if (contains_var) { 
+        char line_copy_var[1024]; // copy of line for tokenization
+        strcpy(line_copy_var, line);
+
+        // remove spaces at beginning
+        char *ptr = line_copy_var;
+        while (isspace(*ptr)) {
+            ptr++;
         }
 
 
-        // construct node
-        MemNode *new_var = malloc(sizeof(MemNode));
+        char *next_var = strchr(ptr, ' ');
+        char *var_name = strtok(next_var, ",;");
+        int num_elements = 1;
+        
+        while (var_name != NULL) {
+            
+            // remove spaces at beginning
+            while (isspace(*var_name)) {
+                var_name++;
+            }
+            // cut out equals and spaces
+            char var_tmp[1024];
+            int i = 0;
+            while (i < strlen(var_name)) {
+                if (var_name[i] == ' ' || var_name[i] == '=') {
+                    var_tmp[i] = '\0';
+                    break;
+                }
+                
+                var_tmp[i] = var_name[i];
+                i++;
+            }
+            var_tmp[i] = '\0';
+            var_name = var_tmp;
 
-        strcpy(new_var->type, type);
-        if (curr_func == NULL) { // if variable is global
-            strcpy(new_var->scope, "global");
+
+            if (strcmp(var_name, "") != 0) {
+                // modify type field
+                if (strstr(line, "[") && strstr(line, "]")) { // if variable is array
+                    strcat(type, "[]");
+                }
+                if (var_name[0] == '*') {
+                    strcat(type, "*");
+                    var_name++;
+                }
+                else if (type[strlen(type) - 1] == ' '){ // remove added space
+                    type[strlen(type) - 1] = '\0';
+                }
+                
+
+                // construct node
+                MemNode *new_var = malloc(sizeof(MemNode));
+
+                strcpy(new_var->type, type);
+                strcpy(new_var->var_name, var_name);
+                new_var->size = getSize(type, num_elements);
+                new_var->next = NULL;
+                
+                if (curr_func == NULL) { // if variable is global
+                    strcpy(new_var->scope, "global");
+                    insertMemNode(static_head, new_var);
+                }
+                else {
+                    curr_func->num_variables++;
+
+                    strcpy(new_var->scope, curr_func->function_name);
+
+                    if (strstr(line, "malloc")) {
+                        char line_malloc[1024];
+                        // strcpy(line_malloc, line);
+                        // char *token = strtok(line_malloc, "(");
+                        // char *size_str = strtok(line_malloc, ")");
+                        // num_elements = atoi(size_str);
+                        insertMemNode(heap_head, new_var);
+                    }
+                    else if (strstr(line, "= \"")) {
+                        char line_malloc[1024];
+                        // strcpy(line_malloc, line);
+                        // char *token = strtok(line_malloc, "(");
+                        // char *size_str = strtok(line_malloc, ")");
+                        // num_elements = atoi(size_str);
+                        insertMemNode(ro_head, new_var);
+                    }
+                    else  {
+                        insertMemNode(stack_head, new_var);
+                    }
+                }
+            }
+            var_name = strtok(NULL, ",;");
         }
-        else {
-            strcpy(new_var->scope, curr_func->function_name);
-        }
-        new_var->next = NULL;
-        insertMemNode(head, new_var);
+        
     }
-    
 
     return contains_var;
 
@@ -157,7 +279,7 @@ bool isVar(char *line, char **types, int num_types, FunctionNode *curr_func, Mem
  * list whose head is head
  * 
  */
-FunctionNode *initFunction(char *header, LLHeads *heads) {
+FunctionNode *initFunction(char *header, FunctionNode *func_head, MemNode *stack_head) {
     
     // copy of header (for later)
     char header_copy[1024];
@@ -173,7 +295,7 @@ FunctionNode *initFunction(char *header, LLHeads *heads) {
     strcpy(new_func->function_name, function_name);
     new_func->next = NULL;
 
-    insertFuncNode(heads, new_func);
+    insertFuncNode(func_head, new_func);
     
     // keep track of paramaters as variables
     char *params = strtok(header_copy, "(");
@@ -182,8 +304,10 @@ FunctionNode *initFunction(char *header, LLHeads *heads) {
 
     char *type;
     char *param_name;
-
+    int num_elements;
     while (params != NULL) {
+        num_elements = 1;
+
         if (params[0] == ' ') {
             params++; // remove first character if it is a space
         }
@@ -200,18 +324,14 @@ FunctionNode *initFunction(char *header, LLHeads *heads) {
         strcpy(new_var->type, type);
         strcpy(new_var->var_name, param_name);
         strcpy(new_var->scope, new_func->function_name);
+        new_var->size = getSize(type, num_elements);
         new_var->next = NULL;
-        insertMemNode(heads->stack_head, new_var);
+        insertMemNode(stack_head, new_var);
         new_func->num_variables++;
 
         params = strtok(NULL, " ");
     }
     
-    MemNode *mp = heads->static_head;
-    while (mp != NULL) {
-        printf("STACK:\t%s\t%s\t%s\t%d\n", mp->var_name, mp->scope, mp->type, mp->size);
-        mp = mp->next;
-    }
 
     return new_func;
 }
@@ -222,7 +342,7 @@ FunctionNode *initFunction(char *header, LLHeads *heads) {
  * Returns 0 iff there is a valid file indicated by user
  * 
  */
-int readFile(Stats *stats, int argc, char **argv, LLHeads *heads) {
+int readFile(Stats *stats, int argc, char **argv, FunctionNode *func_head, MemNode *ro_head, MemNode *static_head, MemNode *heap_head, MemNode *stack_head) {
     
 
     FILE *src_file; // pointer to file with source code
@@ -239,12 +359,7 @@ int readFile(Stats *stats, int argc, char **argv, LLHeads *heads) {
         return 1;
     }
     else {
-        heads->func_head = NULL;
-        heads->ro_head = NULL;
-        heads->static_head = NULL;
-        heads->heap_head = NULL;
-        heads->stack_head = NULL;
-        
+
         char *types[7] = {"void ", "int ", "float ", "char ", "int* ", "float* ", "char* "};
         int num_types = 7;
 
@@ -268,12 +383,12 @@ int readFile(Stats *stats, int argc, char **argv, LLHeads *heads) {
             if (!reading_function) {
                 reading_function = isFunctionHeader(line, types, num_types);
                 if (reading_function == true) {
-                    curr_func = initFunction(line, heads);
+                    curr_func = initFunction(line, func_head, stack_head);
                     stats->num_functions++; // increment num_functions
                 }
                 else {
                     // global variables
-                    is_global_var = isVar(line, types, num_types, curr_func, heads->static_head);
+                    is_global_var = isVar(line, types, num_types, curr_func, ro_head, static_head, heap_head, stack_head);
                 }
             }
             else {
@@ -281,15 +396,13 @@ int readFile(Stats *stats, int argc, char **argv, LLHeads *heads) {
                     curr_func->num_lines++;
                 }
                 
-                if (isVar(line, types, num_types, curr_func, heads->static_head) == true) {
-                    curr_func->num_variables++;
-                }
-
                 // end of function reached
                 if (strstr(line, "}")) {
                     reading_function = false; 
                     curr_func = NULL;
                 }
+
+                isVar(line, types, num_types, curr_func, ro_head, static_head, heap_head, stack_head);
             }
 
             stats->num_lines++;
@@ -297,12 +410,6 @@ int readFile(Stats *stats, int argc, char **argv, LLHeads *heads) {
         }
 
         fclose(src_file); // close file
-
-        MemNode *mp = heads->static_head;
-        while (mp != NULL) {
-            printf("STACK:\t%s\t%s\t%s\t%d\n", mp->var_name, mp->scope, mp->type, mp->size);
-            mp = mp->next;
-        }
         return 0;
     }
 }
@@ -311,7 +418,31 @@ int readFile(Stats *stats, int argc, char **argv, LLHeads *heads) {
  * Prints the output given information read from source file
  * 
  */
-void printOutput(Stats *stats, LLHeads *heads) {
+void printOutput(Stats *stats, FunctionNode *func_head, MemNode *ro_head, MemNode *static_head, 
+                 MemNode *heap_head, MemNode *stack_head) {
+    
+    // free heads of linked lists
+    FunctionNode *temp_f = func_head;
+    func_head = func_head->next;
+    free(temp_f);
+
+    MemNode *temp = ro_head;
+    ro_head = ro_head->next;
+    free(temp);
+
+    temp = static_head;
+    static_head = static_head->next;
+    free(temp);
+
+    temp = heap_head;
+    heap_head = heap_head->next;
+    free(temp);
+
+    temp = stack_head;
+    stack_head = stack_head->next;
+    free(temp);
+    //
+
     printf(">>> Memory Model Layout <<<\n");
 
     printf("***  exec // text ***\n");
@@ -321,23 +452,35 @@ void printOutput(Stats *stats, LLHeads *heads) {
 
     printf("\n### ROData ###       scope  type  size\n");
     // string_literals
+    mp = ro_head;
+    while (mp != NULL) {
+        printf("   %s   %s   %s   %d\n", mp->var_name, mp->scope, mp->type, mp->size);
+        mp = mp->next;
+    }
 
     printf("\n### static data ###\n");
     // global_variables
+    mp = static_head;
+    while (mp != NULL) {
+        printf("   %s   %s   %s   %d\n", mp->var_name, mp->scope, mp->type, mp->size);
+        mp = mp->next;
+    }
 
     printf("\n### heap ###\n");
     // heap variables
+    mp = heap_head;
+    while (mp != NULL) {
+        printf("   %s   %s   %s   %d\n", mp->var_name, mp->scope, mp->type, mp->size);
+        mp = mp->next;
+    }
 
     printf("\n####################\n### unused space ###\n####################\n");
 
     printf("\n### stack ###\n");
     // stack variables
-    mp = heads->stack_head;
-    if (heads->stack_head == NULL) {
-        printf("hi\n");
-    }
+    mp = stack_head;
     while (mp != NULL) {
-        printf("\t%s\t%s\t%s\t%d\n", mp->var_name, mp->scope, mp->type, mp->size);
+        printf("   %s   %s   %s   %d\n", mp->var_name, mp->scope, mp->type, mp->size);
         mp = mp->next;
     }
 
@@ -347,7 +490,7 @@ void printOutput(Stats *stats, LLHeads *heads) {
     FunctionNode *p = NULL; // pointer for traversal
     
     printf("  - Total number of functions: %d\n    ", stats->num_functions);
-    p = heads->func_head;
+    p = func_head;
     while (p != NULL) {
         if (p->next != NULL) {
             printf("%s, ", p->function_name);
@@ -359,14 +502,14 @@ void printOutput(Stats *stats, LLHeads *heads) {
     }
 
     printf("  - Total number of lines per functions:\n");
-    p = heads->func_head;
+    p = func_head;
     while (p != NULL) {
         printf("    %s: %d\n", p->function_name, p->num_lines);
         p = p->next;
     }
 
     printf("  - Total number of variables per function:\n");
-    p = heads->func_head;
+    p = func_head;
     while (p != NULL) {
         printf("    %s: %d\n", p->function_name, p->num_variables);
         p = p->next;
@@ -377,7 +520,11 @@ void printOutput(Stats *stats, LLHeads *heads) {
 
 int main(int argc, char **argv) {
     Stats *stats = malloc(sizeof(Stats));
-    LLHeads *heads = malloc(sizeof(LLHeads));
+    FunctionNode *func_head = malloc(sizeof(FunctionNode));
+    MemNode *ro_head = malloc(sizeof(MemNode));
+    MemNode *static_head = malloc(sizeof(MemNode));
+    MemNode *heap_head = malloc(sizeof(MemNode));
+    MemNode *stack_head = malloc(sizeof(MemNode));
 
     // Initialize fields of stats
     if (argv[1] != NULL)
@@ -385,19 +532,17 @@ int main(int argc, char **argv) {
     stats->num_lines = 0;
     stats->num_functions = 0;
 
-    
-
     // Call functions
-    int validFile = readFile(stats, argc, argv, heads);
+    int validFile = readFile(stats, argc, argv, func_head, ro_head, static_head, heap_head, stack_head);
     if (validFile != 0) {
         return validFile;
     }
     
 
-    printOutput(stats, heads);
+    printOutput(stats, func_head, ro_head, static_head, heap_head, stack_head);
 
     free(stats); // free stats struct
-    free(heads); // free linked list heads
+    // deleteFunctionList(func_head);
 
     return 0;
 }
